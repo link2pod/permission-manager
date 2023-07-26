4f4d160d0e0d7708377a411bc248bf1e7f5716ad
ray <raywang999@gmail.com>
2023-07-26 13:58:16 -0400

    Add Links to Managers

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary": "#570DF8",
+        "secondary": "#F000B8",
+        "accent": "#37CDBE",
+        "neutral": "#090C0D",
+        "base": "#FFFFFF",
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
       },
     },
   },
